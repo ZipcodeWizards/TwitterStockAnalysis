f512bd5f7312f667258a2b9bd7af4f68c1d397c1
Chuck <chuck@zipcodes-MacBook-Pro-chuck.local>
2021-09-09 23:21:42 -0400

    console.py

---
+++ PipeLine/console.py
@@ -0,0 +1,47 @@
+import sqlite3
+from sqlite3 import connect
+from sqlalchemy import create_engine
+import pandas as pd
+import cleaning_spark
+import cleaning_nlp
+import cleaning_tsla_prices
+import plotly_graphs
+
+
+def dates_from_twitter():
+    con = sqlite3.connect('sentiment.db')
+    df = pd.read_sql_query("SELECT DISTINCT date_time FROM sentiment", con)
+    df['date'] = df.date_time.str[:10]
+    twdates = df.date.unique()
+    con.close()
+    return twdates
+
+def dates_from_stock():
+    con = sqlite3.connect('sentiment.db')
+    df = pd.read_sql_query("SELECT DISTINCT date FROM tsla_stock_prices", con)
+    df['date1'] = df.date.str[:10]
+    twdates = df.date1.unique()
+    con.close()
+    return twdates
+
+if __name__ == "__main__":
+    choice = input('')
+    l1 = dates_from_twitter()
+    l2 = dates_from_stock()
+    l3 = []
+    for d in l1:
+        for s in l2:
+            if d == s:
+                l3.append(d)
+    for i in l3:
+        print(i)
+    date = str(input("Pick a date to analize :"))
+    cleaning_spark.main2(date)
+    cleaning_nlp.main3(date)
+    cleaning_tsla_prices.main5(date)
+    plotly_graphs.main6(date)
+    
+
+     
+
+
